f49f865787c72594f4749efba178390ed4e7b871
xingliu <xing.liu@centling.com>
2018-01-11 16:31:51 +0800

    branch version

--- startup.sh
+++ startup.sh
@@ -1,31 +1,18 @@
 #!/bin/sh
+GIT_HOST=`echo $GIT_URL | awk -F: '{print $1}'`
 GITBOOK=gitbook
-if [ ! $GIT_URL ];then  
-   GITBOOK_HOME=$PROJECT
-   echo "\$GIT_URL IS NULL use local $GITBOOK_HOME"  
-else  
-   GITBOOK_HOME=$PROJECT/$GITBOOK
-   git config --global user.email "gitbook@example.com"
-   git config --global user.name "gitbook"
-   git config --global core.sshCommand "ssh -o StrictHostKeyChecking=no -F /dev/null"
-   expect -c "
-    set timeout 1;
-    spawn git clone $GIT_URL $PROJECT
-    expect \"(yes/no)?\" {send \"yes\n\"};
-    expect *# {send exit\n};
-    expect eof;"
-   #init gitbook dir
-   if [ ! -d "$GITBOOK_HOME" ];then
-		mkdir -p $GITBOOK_HOME
-		cd $PROJECT
-cat >$GITBOOK/README.md <<-!
+BRANCH=gitbook
+
+gitbook_init() {
+cat >README.md <<-! 
 # README
 
 This is a book powered by [GitBook](https://github.com/GitbookIO/gitbook).
-Try to edit markdown files by [Markdown Editor](https://jbt.github.io/markdown-editor/). 
+Try to edit markdown files by [Markdown Editor](https://jbt.github.io/markdown-editor/).
 Or create a gitbook by [Gitbook Editor](https://www.gitbook.com/editor).
 !
-cat >$GITBOOK/SUMMARY.md <<-!
+
+cat >SUMMARY.md <<-!
 # SUMMARY
 
 * [Chapter1](chapter1/README.md)
@@ -33,11 +20,45 @@ cat >$GITBOOK/SUMMARY.md <<-!
 * [Section1.2](chapter1/section1.2.md)
 * [Chapter2](chapter2/README.md)
 !
-      git add -A && git commit -m "make gitbook dir" && git pull --rebase && git push
+}
+
+
+if [ ! $GIT_URL ];then  
+   echo "\$GIT_URL IS NULL use local $PROJECT"  
+else
+   git config --global user.email "gitbook@example.com"
+   git config --global user.name "gitbook"
+   git config --global core.sshCommand "ssh -o StrictHostKeyChecking=no -F /dev/null"
+   rm -rf /root/.ssh/known_hosts
+   echo "======rm known_hosts=========="
+   
+   expect -c "
+    set timeout 1;
+    spawn ssh $GIT_HOST; 
+    expect \"(yes/no)?\" {send \"yes\n\"};
+    expect *# {send exit\n};
+    expect eof;" >/tmp/log
+    
+   git clone -b $BRANCH $GIT_URL $PROJECT
+   
+   #branch not exist,need to create a new branch named gitbook
+   if [ $? != 0 ];then
+        echo "========create branch=============="
+        rm -rf $PROJECT
+	git clone $GIT_URL $PROJECT
+        cd $PROJECT
+        git checkout -b $BRANCH
+        rm -rf *
+        gitbook_init
+        echo "==========git commit=============================="
+        git commit -am "init gitbook"
+        #git pull origin $BRANCH --rebase
+        git push origin $BRANCH
    fi
+   echo "===============end=========================================="   
    #add a cronjob to get git changed files
    [ ! "$INTERVAL" ] && INTERVAL=5
-   echo "*/$INTERVAL * * * * cd $PROJECT && git pull --rebase" > /var/spool/cron/crontabs/root
+   echo "*/$INTERVAL * * * * cd $PROJECT && git pull origin $BRANCH --rebase" > /var/spool/cron/crontabs/root
    crond
 fi  
-cd $GITBOOK_HOME && gitbook serve
+cd $PROJECT && gitbook serve
--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 >/root/.ssh/known_hosts
 docker rm -f gitbook
-#docker run --name gitbook -it -p 4000:4000 -e GIT_URL=git@git.eng.centling.com:testing -v ~/.ssh:/root/.ssh liuxing1981/gitbook sh
-docker run --name gitbook -d -p 4000:4000 -e GIT_URL=git@git.eng.centling.com:testing -v ~/.ssh/id_rsa:/root/.ssh/id_rsa -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub liuxing1981/gitbook
+docker run --name gitbook -it -p 4000:4000 -e GIT_URL=git@git.eng.centling.com:testing \
+	-v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+	-v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
+	-v /home/figo/shells/docker/gitbook/startup.sh:/root/startup.sh \
+	liuxing1981/gitbook sh
+#docker run --name gitbook -d -p 4000:4000 -e GIT_URL=git@git.eng.centling.com:testing \
+#	-v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+#	-v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
+#	liuxing1981/gitbook
